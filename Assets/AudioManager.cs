ce0f833b3ad835abf878130d281d61217fb9f916
Pogromel <Binkowski-areek@o2.pl>
2022-04-22 01:45:27 +0100

    22/04/2022

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -1,10 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.Audio;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-
+    public Sound[] sounds;
    
    
 }
