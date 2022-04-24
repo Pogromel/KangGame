4c59d8499e967e77393cb04ee1b56221cd9d7261
Pogromel <Binkowski-areek@o2.pl>
2022-04-24 01:47:45 +0100

    24/04/2022 level 4 completed!

---
+++ Assets/Scripts/Finish2.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Finish2 : MonoBehaviour
+{
+
+    private AudioSource finishSound;
+
+    private bool levelCompleted = false;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        finishSound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player" && !levelCompleted)
+        {
+            finishSound.Play();
+            Invoke("CompleteLevel", 1.5f);
+
+        }
+    }
+
+    public  void CompleteLevel()
+    {
+        SceneManager.LoadScene("BonusLevel");
+    }
+}
